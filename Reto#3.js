0e5cc85657310a5d7b70873508de2dbee521037b
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2022-12-05 16:51:24 -0300

    Reto #3: ¿Cuántas cajas de regalos puede llevar Papá Noel?

--- Reto#3.js
+++ Reto#3.js
@@ -31,8 +31,4 @@ distributeGifts = (packOfGifts, reindeers) => {
 const packOfGifts = ["book", "doll", "ball"]
 const reindeers = ["dasher", "dancer"]
 
-// el pack de regalos pesa 4 + 4 + 4 = 12
-// los renos pueden llevar (2 * 6) + (2 * 6) = 24
-// por lo tanto, Santa Claus puede entregar 2 cajas de regalos
-
-distributeGifts(packOfGifts, reindeers) // 2
+console.log(distributeGifts(packOfGifts, reindeers))
