ba6503159c2f904ea91cb9eaf419d6fe8ac0b8af
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2022-12-05 16:55:18 -0300

    Reto #4: Una caja dentro de otra caja y otra...

--- Reto#4.js
+++ Reto#4.js
@@ -53,3 +53,10 @@ function fitsInOneBox(boxes) {
     });
   }
 
+  const boxes = [
+    { l: 1, w: 1, h: 1 },
+    { l: 3, w: 3, h: 3 },
+    { l: 2, w: 2, h: 2 }
+  ]
+
+console.log(fitsInOneBox(boxes));
