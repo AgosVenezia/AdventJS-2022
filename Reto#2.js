9ff0305980e461ac289a889a82ae7e0f7c087b15
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2022-12-27 15:06:10 -0300

    Reto #2: Nadie quiere hacer horas extra

--- Reto#2.js
+++ Reto#2.js
@@ -25,9 +25,6 @@ El año puede ser bisiesto. Haz las comprobaciones que necesitas para ello, si f
 Aunque el 31 de diciembre sea festivo, las horas extra se harán el mismo año y no el siguiente.
 El método Date.getDay() te devuelve el día de la semana de una fecha. El 0 es domingo, el 1 es lunes, etc. */
 
-const year = 2022
-const holidays = ['01/06', '04/01', '12/25']
-
 function countHours(year, holidays) {
     let total = 0;
     holidays.forEach(holiday => {
@@ -40,4 +37,6 @@ function countHours(year, holidays) {
     return total
   }
 
+const year = 2022
+const holidays = ['01/06', '04/01', '12/25']
 console.log(countHours(year, holidays));
