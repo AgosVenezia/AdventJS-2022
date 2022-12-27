53ba03b1e3764c30e5c4e07b4d9b64ceab44bf32
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2022-12-27 15:31:53 -0300

    Reto #22: La iluminación en sintonía

--- Reto#22.js
+++ Reto#22.js
@@ -33,19 +33,19 @@ La posición del nombre del sistema en systemNames y el número de paso en stepN
 Los pasos en stepNumbers pueden repetirse para diferentes sistemas. */
 
 function checkStepNumbers(systemNames, stepNumbers) {
-    let hashMap = new Map();
-  
-    return systemNames.every(
-      (name, i, flag, value) => (
-        (value = hashMap.get(name)),
-        (flag = !!(!value && hashMap.set(name, [stepNumbers[i]]))),
-        !flag &&
-          ((flag = value.at(-1) < stepNumbers[i]), flag) &&
-          value.push(stepNumbers[i]),
-        flag
-      )
-    );
-  }
+  let hashMap = new Map();
+
+  return systemNames.every(
+    (name, i, flag, value) => (
+      (value = hashMap.get(name)),
+      (flag = !!(!value && hashMap.set(name, [stepNumbers[i]]))),
+      !flag &&
+        ((flag = value.at(-1) < stepNumbers[i]), flag) &&
+        value.push(stepNumbers[i]),
+      flag
+    )
+  );
+}
 
 const systemNames = ["tree_1", "tree_2", "house", "tree_1", "tree_2", "house"]
 const stepNumbers = [1, 33, 10, 2, 44, 20]
